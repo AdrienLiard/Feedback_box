731c8586b35ff29443be5df014f6827208d5a5e0
Adrien LIARD <adrien.liard@inference-operations.com>
2015-06-29 20:12:30 +0200

    Correction questionnaire

--- FeedBack_Box/FeedBack_Box.py
+++ FeedBack_Box/FeedBack_Box.py
@@ -20,7 +20,7 @@ DEBUG=True
 PORT=80
 HOST='0.0.0.0'
 SECRET_KEY = 'MySuperSecretKey'
-ROOT_URL="http://bvabox.fr"
+ROOT_URL="http://bvabox.fr/"
 #-------------------------------
 
 #-------------------------------
